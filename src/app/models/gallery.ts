bac475521da4100d8baae50f411770a9155a72ac
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-03-04 19:10:01 +0100

    feat: Update images

--- src/app/models/gallery.ts
+++ src/app/models/gallery.ts
@@ -1,17 +1,17 @@
 export const images = [
   {
     url: 'assets/images/st.luke.7.sculpture.outside.church.resized.jpg',
-    description: '',
+    description: 'Sculpture outside church auditorium.',
     thumbnail: ''
   },
   {
     url: 'assets/images/st.luke.9.group.picture.with.all.members.of.the.congregation.resized.jpg',
-    description: '',
+    description: 'Group picture with many members of the congregation.',
     thumbnail: ''
   },
   {
     url: 'assets/images/st.luke.10.church.altar.resized.jpg',
-    description: '',
+    description: 'Church altar',
     thumbnail: ''
   },
 ];
