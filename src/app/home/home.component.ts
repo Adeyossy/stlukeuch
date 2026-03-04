04a7069c034ab3063f417dcd669d6ceb38b35a3a
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-03-04 19:12:02 +0100

    feat: Style website home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { images } from '../models/gallery';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  imgs = images;
 }
