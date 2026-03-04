42b46047707cd3d50de5a174c2bd9ee375dc97d0
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-03-04 19:40:19 +0100

    feat: Generate support component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
+import { SupportComponent } from './support/support.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavComponent
+    NavComponent,
+    SupportComponent
   ],
   imports: [
     BrowserModule,
