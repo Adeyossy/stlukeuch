f80ad483eed62d585f865d0f002563b0cfe3f964
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-03-09 09:43:00 +0100

    feat: Configure support route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SupportComponent } from './support/support.component';
 
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent
+  },
+  {
+    path: "support/:category",
+    component: SupportComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      {
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'top'
+      }
+    )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
