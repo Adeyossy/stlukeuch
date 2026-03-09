58c016a6883d5a856e9e7274251ae4b5ef0ccfb3
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-03-09 13:35:59 +0100

    feat: Generate page 5 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
 import { SupportComponent } from './support/support.component';
 import { FormsModule } from '@angular/forms';
+import { Page5Component } from './page-5/page-5.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavComponent,
-    SupportComponent
+    SupportComponent,
+    Page5Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/page-5/page-5.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-5',
+  templateUrl: './page-5.component.html',
+  styleUrls: ['./page-5.component.css']
+})
+export class Page5Component {
+
+}
