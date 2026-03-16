39e253cfeaa9956e2d53a0b5570a13a1e1795a96
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-03-16 09:38:36 +0100

    feat: Configure page 6

--- src/app/page-6/page-6.component.ts
+++ src/app/page-6/page-6.component.ts
@@ -6,5 +6,148 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-6.component.css']
 })
 export class Page6Component {
-
+  sections = [
+    {
+      title: "Weekly Mass Time Table",
+      items: [
+        {
+          heading: "",
+          subheadings: [
+            ""
+          ]
+        },
+        {
+          heading: "",
+          subheadings: [
+            ""
+          ]
+        },
+      ]
+    },
+    {
+      title: "Sunday Mass Time Table",
+      items: [
+        {
+          heading: "Second Sundays (Community Sunday):",
+          subheadings: [
+            "9am and 6pm",
+            "Family time holds after the 9am Mass"
+          ]
+        },
+        {
+          heading: "Other Sundays:",
+          subheadings: [
+            "6:30am, 9:00am and 6:00pm"
+          ]
+        },
+      ]
+    },
+    {
+      title: "Meeting times for society activities",
+      items: [
+        {
+          heading: "",
+          subheadings: [
+            ""
+          ]
+        }
+      ]
+    },
+    {
+      title: "Confession time",
+      items: [
+        {
+          heading: "",
+          subheadings: [
+            ""
+          ]
+        }
+      ]
+    },
+    {
+      title: "Visit to the Parish Priest",
+      items: [
+        {
+          heading: "",
+          subheadings: [
+            ""
+          ]
+        }
+      ]
+    },
+    {
+      title: "Important phone numbers - Society leads",
+      items: [
+        {
+          heading: "",
+          subheadings: [
+            ""
+          ]
+        }
+      ]
+    },
+    {
+      title: "Important phone numbers - Society leads",
+      items: [
+        {
+          heading: "",
+          subheadings: [
+            ""
+          ]
+        }
+      ]
+    },
+    {
+      title: "Contact persons - Chaplaincy Pastoral Council Members",
+      items: [
+        {
+          heading: "Chairman of CPC",
+          subheadings: [
+            ""
+          ]
+        },
+        {
+          heading: "Chairman of Chaplaincy Committee",
+          subheadings: [
+            ""
+          ]
+        },
+        {
+          heading: "Welfare Lead",
+          subheadings: [
+            ""
+          ]
+        },
+        {
+          heading: "Project Lead",
+          subheadings: [
+            ""
+          ]
+        },
+        {
+          heading: "Education Lead",
+          subheadings: [
+            ""
+          ]
+        },
+        {
+          heading: "IT Lead",
+          subheadings: [
+            ""
+          ]
+        },
+      ]
+    },
+    {
+      title: "Opening hours of the Church",
+      items: [
+        {
+          heading: "",
+          subheadings: [
+            ""
+          ]
+        }
+      ]
+    },
+  ]
 }
