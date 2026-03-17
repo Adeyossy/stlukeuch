9aa48090bd80c0af71a16f96eb88d472550c2274
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-03-17 15:15:17 +0100

    feat: Configure page 8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SupportComponent } from './support/support.component';
+import { Page5Component } from './page-5/page-5.component';
+import { Page6Component } from './page-6/page-6.component';
+import { Page8Component } from './page-8/page-8.component';
 
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent
+  },
+  {
+    path: "support",
+    component: Page5Component
+  },
+  {
+    path: "practical-information",
+    component: Page6Component
+  },
+  {
+    path: "the-catholic-faith",
+    component: Page8Component
   },
   {
     path: "support/:category",
--- src/app/page-8/page-8.component.ts
+++ src/app/page-8/page-8.component.ts
@@ -6,5 +6,96 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-8.component.css']
 })
 export class Page8Component {
-
+  sections = [
+    {
+      title: "Catechism Activities",
+      subtitle: "Please register with the Parish Priest. Attendance is monitored for receipt of the sacraments. Open to all who want to refresh their memory on the faith.",
+      items: [
+        {
+          heading: "For Children",
+          subheadings: [
+            "Saturday at 4pm"
+          ]
+        },
+        {
+          heading: "For Adults",
+          subheadings: [
+            "Saturday at 4pm"
+          ]
+        },
+      ]
+    },
+    {
+      title: "Chaplaincy Library",
+      subtitle: "Access to teachings from:",
+      items: [
+        {
+          heading: "Health Workers Guild meetings",
+          subheadings: []
+        },
+        {
+          heading: "Catholic Physicians Guild meetings",
+          subheadings: []
+        },
+        {
+          heading: "Guild of Nurse meeting",
+          subheadings: []
+        },
+        {
+          heading: "Legion of Mary",
+          subheadings: []
+        },
+        {
+          heading: "NFCS",
+          subheadings: []
+        },
+        {
+          heading: "FECAMDS",
+          subheadings: []
+        }
+      ]
+    },
+    {
+      title: "Guide of source for Catholic Faith Teaching",
+      subtitle: "",
+      items: [
+        {
+          heading: "ETWN Teaching",
+          subheadings: [
+            "https://www.ewtn.com/catholicism/teachings"
+          ]
+        },
+        {
+          heading: "Vatican Documents",
+          subheadings: [
+            "https://www.vatican.va/content/vatican/en.html"
+          ]
+        },
+        {
+          heading: "10 minutes with Jesus App",
+          subheadings: [
+            "https://10minuteswithjesus.org/"
+          ]
+        },
+        {
+          heading: "Laudate App",
+          subheadings: [
+            "https://play.google.com/store/apps/details?id=com.aycka.apps.MassReadings&hl=en&gl=US"
+          ]
+        },
+        {
+          heading: "Catechism of the Catholic Church",
+          subheadings: [
+            "https://www.vatican.va/archive/ENG0015/_INDEX.HTM"
+          ]
+        },
+        {
+          heading: "Catholic News Agency",
+          subheadings: [
+            "https://www.catholicnewsagency.com/news/28886/new-app-aims-to-enliven-scriptures-boost-accessibility"
+          ]
+        }
+      ]
+    }
+  ];
 }
