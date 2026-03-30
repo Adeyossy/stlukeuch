1f273cce8cecd85cffdfbb7392398ad07624a0d9
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-03-30 09:31:27 +0100

    feat: Include page 7 pictures

--- src/app/page-7/page-7.component.ts
+++ src/app/page-7/page-7.component.ts
@@ -9,8 +9,9 @@ import { Section } from '../models/section';
 export class Page7Component {
   sections: Section[] = [
     {
-      title: "",
+      title: "Prayer Time, Legion of Mary & More",
       subtitle: "",
+      body: [],
       images: [],
       components: [
         {
@@ -62,39 +63,99 @@ export class Page7Component {
         },
       ],
       action: {
-        text: "JOIN THE WORKFORCE",
-        url: "https://wa.me/2348066199175"
+        text: "",
+        url: ""
       }
     },
     {
       title: "Baptism",
-      subtitle: "Infant baptism is available. It may be given in the Church or on the Wards when necessary.",
+      subtitle: "",
+      body: [
+        "Infant baptism is available. It may be given in the Church or on the Wards when necessary.",
+        "Adult baptism is also available, given with the sacrament of the Sacrament of the Holy Eucharist together with or without the scarcement of Confirmation.",
+        "Please contact the Parish Priest for the logistics required to receive this sacrament by sending a message.",
+        "Please include: your names, your parish or previous faith, your preferred phone number for communication, your specific request."
+      ],
       components: [],
       images: [
         {
-          url: "assets/images/st.luke.young.choir.jpg",
-          description: "The Choir"
+          url: "assets/images/baby.baptism.png",
+          description: "Baby Baptism"
         },
         {
-          url: "assets/images/st.luke.reverend.holding.an.emblem.png",
-          description: "Church Ushers"
-        },
-        {
-          url: "assets/images/st.luke.church.helper.and.priest.jpg",
-          description: "Alter Servers"
-        },
-        {
-          url: "assets/images/st.luke.prayer.1.png",
-          description: "Lectors"
-        },
-        {
-          url: "assets/images/st.luke.sacristans.jpg",
-          description: "Sacristans"
-        },
+          url: "assets/images/communion.png",
+          description: "Communion"
+        }
       ],
       action: {
-        text: "JOIN THE WORKFORCE",
+        text: "SEND MESSAGE",
         url: "https://wa.me/2348066199175"
+      }
+    },
+    {
+      title: "Marriage",
+      subtitle: "",
+      body: [
+        "Marriage classes are available physically.",
+        "When the intended are outside of Nigeria, we can support your marriage class through video conferencing (video must be on).",
+        "If you are engaging in a mixed marriage and require teaching in other sacraments, we can also support you though our catechism classes. Please see that section for more information.",
+        "Please contact the Parish Priest for the logistics required to receive this sacrament by sending a message.",
+        "Please include: your names, your parish or previous faith, your preferred phone number for communication, your specific request."
+      ],
+      components: [],
+      images: [
+        {
+          url: "assets/images/baby.baptism.png",
+          description: "Baby Baptism"
+        },
+        {
+          url: "assets/images/communion.png",
+          description: "Communion"
+        }
+      ],
+      action: {
+        text: "SEND MESSAGE",
+        url: "https://wa.me/2348066199175"
+      }
+    },
+    {
+      title: "Funerals, Catechism & Confession",
+      subtitle: "",
+      body: [],
+      images: [],
+      components: [
+        {
+          title: "Funerals",
+          body: [],
+          link: "",
+          type: "",
+          action: ""
+        },
+        {
+          title: "Catechism",
+          body: [
+            "There is catechism for children on Saturdays at 4 pm in the Church hall.",
+            "We also have children’s mass guided by a religious to optimise their learning and participation during the Holy Mass.",
+            "There is catechism for adults on Saturday from 4 - 6pm in an office within the Church hall.",
+          ],
+          link: "",
+          type: "",
+          action: ""
+        },
+        {
+          title: "Confession",
+          body: [
+            "Confession are available every day after morning Mass at 6:30 am and at 8:00 am on Saturdays.",
+            "Also available after 9 am Mass on Sundays."
+          ],
+          link: "",
+          type: "",
+          action: ""
+        }
+      ],
+      action: {
+        text: "",
+        url: ""
       }
     }
   ];
